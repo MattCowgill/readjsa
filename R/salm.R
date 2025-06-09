47a8200cbaee089ce5a97f6363414e560302a748
Matt Cowgill <mattcowgill@gmail.com>
2025-06-09 12:39:55 +1000

    fix read_salm()

--- R/salm.R
+++ R/salm.R
@@ -5,7 +5,7 @@
 #' @param geog_type A character specifying geography data type
 #' Possible options are `c("LGA", "SA2")`
 #' @param path Path to the directory where the file(s) should be saved
-#' @returns A tibble/list of tibbles with the SALM data, in 'long'/tidy format
+#' @returns A tibble with the SALM data, in 'long'/tidy format
 #' @export
 #' @examples
 #' \dontrun{
@@ -35,7 +35,7 @@ read_salm <- function(tables = "all",
   # Download if not downloaded already
   if (file.exists(temp_file_loc) == FALSE){
     urls <- get_salm_urls()
-    dl_salm(urls[geog_type], temp_file_loc)
+    dl_file(urls[grep(tolower(geog_type), urls)], temp_file_loc)
   }
 
   # Process and return the data
@@ -46,48 +46,42 @@ read_salm <- function(tables = "all",
 get_salm_urls <- function() {
 
   # Check SALM url for links to excel
-  req <- httr2::request("https://www.jobsandskills.gov.au/data/small-area-labour-markets") |>
+  req <- httr2::request("https://www.dewr.gov.au/employment-research/small-area-labour-markets") |>
     httr2::req_options(http_version=2) # Set to HTTP 1.1
   resp <- httr2::req_perform(req) # Perform request
 
   resp_html <- httr2::resp_body_string(resp) # Convert response to string
   rvest_html <- rvest::minimal_html(resp_html) # Convert to rvest-parsable html
 
-  links <- rvest_html |> rvest::html_elements("a") |> rvest::html_attr("href") # Get character vector of links
-  csv_files <- grep(".csv$", links, value = TRUE) # Extract .csv files
+  links <- rvest_html |>
+    rvest::html_elements("a") |>
+    rvest::html_attr("href")
 
-  urls <- paste0("https://www.jobsandskills.gov.au", c(
-    grep("LGA", csv_files, value = TRUE), # Identify .csv file with LGA data
-    grep("SA2", csv_files, value = TRUE)  # Identify .csv file with SA2 data
-  ))
-  names(urls) <- c("LGA", "SA2")
+  datafile_page_url_fragments <- links[grep("datafiles", links)]
+
+  datafile_page_urls <- paste0("https://www.dewr.gov.au",
+                               datafile_page_url_fragments)
+
+  get_csv_url <- function(page_url) {
+    links_on_page <- page_url |>
+      httr2::request() |>
+      httr2::req_options(http_version=2) |>
+      httr2::req_perform() |>
+      httr2::resp_body_string() |>
+      rvest::minimal_html() |>
+      rvest::html_elements("a") |>
+      rvest::html_attr("href")
+
+    paste0("https://www.dewr.gov.au",
+           links_on_page[grep("csv", links_on_page)])
+  }
+
+  urls <- purrr::map_chr(
+    datafile_page_urls,
+    get_csv_url
+  )
+
   return(urls)
-}
-
-# Function to safely download SALM data
-dl_salm <- function(url, path, quiet = TRUE){
-
-  # Check connection
-  check_jsa_connection()
-
-  # Get latest result
-  latest_url_result <- suppressWarnings(safely_dl(
-    url,
-    destfile = path
-  ))
-
-  # If there's any result, return it
-  if (is.null(latest_url_result$error)) {
-    if (quiet == FALSE){
-      cat("Successfully downloaded data from:\n", utils::URLdecode(url), "\n")
-    }
-  }
-
-  # Otherwise, return the error
-  else {
-    cat("Could not download SALM file from url: ", utils::URLdecode(url), "\n")
-    return(latest_url_result$error)
-  }
 }
 
 # Function to process the downloaded data selected from wide to long
@@ -95,11 +89,18 @@ process_salm <- function(file_path, tables = "all"){
 
   # Convenience function to convert character numbers to numeric
   chr2num <- function(x){
-    as.numeric(gsub(",", "", dplyr::na_if(x, "-")))
+    if (is.numeric(x)) {
+      x
+    } else {
+      as.numeric(gsub(",", "", dplyr::na_if(x, "-")))
+    }
+
   }
 
-  # Read in the wide data, converting date columns to numeric
-  salm_dat <- readr::read_csv(file_path, skip = 1, show_col_types = FALSE) |>
+  # Read in the wide data
+  raw_dat <- readr::read_csv(file_path, skip = 1, show_col_types = FALSE)
+   # convert date columns to numeric
+  salm_dat <- raw_dat |>
     dplyr::mutate(
       dplyr::across(
         .cols = dplyr::matches(".*\\d+$", perl = TRUE),
@@ -148,10 +149,8 @@ process_salm <- function(file_path, tables = "all"){
     }
   )
 
-  # Return either a tibble or list of tibbles
-  if (length(salm_dat_tidy) == 1){
-    return(salm_dat_tidy[[1]]) # Unpack so the result is not a list
-  } else {
-    return(salm_dat_tidy)
-  }
+  # Return a tibble
+  salm_dat_comb <- salm_dat_tidy |>
+    dplyr::bind_rows()
+  return(salm_dat_comb)
 }
--- tests/testthat/test-salm.R
+++ tests/testthat/test-salm.R
@@ -2,23 +2,9 @@ test_that("get_salm_urls () works", {
   urls <- get_salm_urls()
 
   expect_type(urls, "character")
-  expect_true(all(tools::file_ext(urls) == "csv"))
   expect_length(urls, 2)
 })
 
-test_that("dl_salm() works", {
-  skip_if_offline()
-  skip_on_ci()
-  check_jsa_connection()
-
-  urls <- get_salm_urls()
-  for (geog_type in c("LGA", "SA2")){
-    salm_file_path <- tempfile(fileext = ".csv")
-    expect_false(file.exists(salm_file_path))
-    salm_file <- dl_salm(urls[geog_type], path = salm_file_path)
-    expect_true(file.exists(salm_file_path))
-  }
-})
 
 test_that("read_salm() works", {
   skip_if_offline()
